2c55a2eb1737f316953994156d406848831988e1
Steplon <43827083+Steplon@users.noreply.github.com>
2020-03-01 19:11:22 +0000

    Add project files.

---
+++ Areas/Identity/Data/DeliveryPlanningUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DeliveryPlanning.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the DeliveryPlanningUser class
+    public class DeliveryPlanningUser : IdentityUser
+    {
+
+        public string AccountType { get; set; }
+
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DeliveryPlanning.Areas.Identity.Data;
+using DeliveryPlanning.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DeliveryPlanning.Areas.Identity.IdentityHostingStartup))]
+namespace DeliveryPlanning.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DeliveryPlanningContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DeliveryPlanningContextConnection")));
+
+                services.AddDefaultIdentity<DeliveryPlanningUser>()
+                    .AddEntityFrameworkStores<DeliveryPlanningContext>();
+            });
+        }
+    }
+}
